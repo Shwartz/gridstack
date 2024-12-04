e0b8e9913e992e9d31ec58f45d69a82da285830f
andris.svarcs <andris.svarcs@yahoo.com>
2024-12-04 07:38:16 +0000

    add sample

--- src/components/GridWidget.js
+++ src/components/GridWidget.js
@@ -2,11 +2,26 @@ import React, {useEffect, useRef} from "react";
 import {GridStack} from "gridstack";
 import '/node_modules/gridstack/dist/gridstack.min.css';
 
+const CompA = () => {
+  return (
+    <div className="grid-stack-item-content">Tiny A component</div>
+  )
+}
+
 const items = [
   {content: 'my first widget'}, // will default to location (0,0) and 1x1
   {w: 2, content: 'another longer widget!'}, // will be placed next at (1,0) and 2x1
   {h: 2, content: 'Some more'},
+  {content: <CompA/>}
 ];
+
+const serializedData = [
+  {x: 0, y: 0, w: 2, h: 2, content: 'C1 -> x: 0, y: 0, w: 2, h: 2'},
+  {x: 2, y: 3, w: 3, content: 'C2 -> x: 2, y: 3, w: 3'},
+  {x: 1, y: 3, content: 'C3 -> x: 1, y: 3'}
+];
+
+
 
 export function GridWidget() {
   const gridRef = useRef(null);
@@ -15,7 +30,7 @@ export function GridWidget() {
     console.log({gridRef});
     if (gridRef) {
       const grid = GridStack.init();
-      grid.load(items);
+      grid.load(serializedData);
     }
 
   }, []);
